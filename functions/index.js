34bcaba8960f712c2e4b6989a45cf97b75504370
karunvee <you@example.com>
2024-02-14 10:53:13 +0700

    Update Full Functional

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,12 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 const request = require("request-promise");
 const template = require("./template");
 
 const runtimeOpts = {timeoutSeconds: 8, memory: "1GB", minInstances: 1};
 const region = "asia-northeast1";
+
+admin.initializeApp();
 
 const LINE_MESSAGING_API = "https://api.line.me/v2/bot";
 const CHANNEL_ACCESS_TOKEN = "NfKwKCdDVScsuwyliUHIzVJLW1LEMJ2AuCcMZ2aihxEvl8M4gaRHIv5xIRC0TryaMEwiKwIvhhqu09QRjhVQKEI0IrVZtfqA4Q4876epmQ+ylX+cbtKqV8Im/I5C61oI/9jMeYM/6BiuQ6kKmmyjpQdB04t89/1O/w1cDnyilFU=";
@@ -21,34 +24,71 @@ const data_tester = {
 };
 // 🚨⚠️🛑
 
-exports.LineBot = functions.https.onRequest((req, res) => {
+exports.LineBot = functions.https.onRequest(async (req, res) => {
     if (req.method === "POST") {
         const destination = req.body.destination;
-        const event = req.body.events[0];
 
         if (destination === "ACCESS_POINT") {
+            const userIds = await getUserId();
+            await multicast(
+                userIds,
+                {
+                    type: "flex",
+                    // eslint-disable-next-line camelcase
+                    altText: `⚠️🚨⚠️ ${req.body.events[0].data.topic} ⚠️🚨⚠️`,
+                    contents: template.msgAlert(req.body.events[0].data)
+                }
+            );
             res.status(200).send("ACCESS_POINT");
         } else {
-            const event = req.body.events[0];
-            if (event.message.text === "Example01") {
-                reply(
-                    event.replyToken,
-                    {
-                        type: "flex",
-                        // eslint-disable-next-line camelcase
-                        altText: `⚠️🚨⚠️ ${data_tester.topic} ⚠️🚨⚠️`,
-                        contents: template.msgAlert(data_tester)
-                    }
-                );
-            }
+            const events = req.body.events;
+
+            events.forEach(async (event) => {
+                switch (event.type) {
+                    case "follow":
+                        await storeUserId(event.source.userId);
+                        break;
+                    case "unfollow":
+                        await removeUserId(event.source.userId);
+                        break;
+                    case "message":
+                        if (event.message.text === "Example") {
+                            await reply(
+                                event.replyToken,
+                                {
+                                    type: "flex",
+                                    // eslint-disable-next-line camelcase
+                                    altText: `🚨 ${data_tester.topic} 🚨`,
+                                    contents: template.msgAlert(data_tester)
+                                }
+                            );
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            });
         }
     }
 
     res.status(200).send("OK");
 });
-const multicast = (token, payload) => {
-    request.post({
+
+const multicast = async (userIds, payload) => {
+    await request.post({
         uri: `${LINE_MESSAGING_API}/message/multicast`,
+        headers: LINE_HEADER,
+        body: JSON.stringify({
+            to: userIds,
+            messages: [payload]
+        })
+    });
+};
+
+
+const reply = async (token, payload) => {
+    await request.post({
+        uri: `${LINE_MESSAGING_API}/message/reply`,
         headers: LINE_HEADER,
         body: JSON.stringify({
             replyToken: token,
@@ -57,14 +97,30 @@ const multicast = (token, payload) => {
     });
 };
 
+const storeUserId = async (userId) => {
+    const firestore = admin.firestore();
+    const usersCollection = firestore.collection("users");
 
-const reply = (token, payload) => {
-    request.post({
-        uri: `${LINE_MESSAGING_API}/message/reply`,
-        headers: LINE_HEADER,
-        body: JSON.stringify({
-            replyToken: token,
-            messages: [payload]
-        })
+    await usersCollection.doc(userId).set({
+      addedAt: admin.firestore.FieldValue.serverTimestamp()
     });
+};
+
+const removeUserId = async (userId) => {
+    const firestore = admin.firestore();
+    const usersCollection = firestore.collection("users");
+
+    await usersCollection.doc(userId).delete();
+};
+
+
+const getUserId = async () => {
+    const firestore = admin.firestore();
+    const usersCollection = firestore.collection("users");
+    const querySnapshot = await usersCollection.get();
+    const userIds = [];
+    querySnapshot.forEach((doc) => {
+      userIds.push(doc.id);
+    });
+    return userIds;
 };
--- readme.txt
+++ readme.txt
@@ -17,3 +17,53 @@ cd to functions/
 deploy **
 cd to functions/
 >>firebase deploy --only functions
+
+
+
+Example payload **
+{
+	"destination": "U69594a0b3a29625a274d7236894f5690",
+	"events": [
+		{
+			"type": "message",
+			"message": {
+				"type": "text",
+				"id": "494892294638403651",
+				"quoteToken": "yXU1cdjoF09M9fRiSopcHvdN-KnGR7QcFH3H2ypSBR1WqxMc0kkReijVPF5bkEh_FJe-64ZiJn6nEskJLRemCp2eC8HRILciDJkbukT4FccxO0fwnwmnVNwB1H3S6BDgPbuxeRHinOyKydN3qkiwsA",
+				"text": "H"
+			},
+			"webhookEventId": "01HPGPM3M6W6Y4XRARTMV9ZMH",
+			"deliveryContext": {
+				"isRedelivery": false
+			},
+			"timestamp": 1707810098309,
+			"source": {
+				"type": "user",
+				"userId": "Ucd78e4d180dac11185673768f387b873"
+			},
+			"replyToken": "957196a8da354deaa818063b1be0606b",
+			"mode": "active"
+		}
+	]
+}
+
+
+
+POST * Sending multicast flex message
+https://us-central1-thongthai-linebot.cloudfunctions.net/LineBot
+
+{
+	"destination": "ACCESS_POINT",
+	"events": [
+		{
+			"type": "Form",
+			"data": {
+                "topic": "flooded2",
+                "building": "Tower A",
+                "location": "Office",
+                "reporter": "87778, John Sintsom",
+                "taskId": "taskId1233"
+			}
+		}
+	]
+}
